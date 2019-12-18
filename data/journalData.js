552a4f73a20167efe886803bd187fb53a79d2232
Henry Tum <tujungahenry@gmail.com>
2019-12-17 20:27:30 -0800

    final touches

---
+++ data/journalData.js
@@ -0,0 +1,21 @@
+// Store the data in an array.
+var journalArray = [
+    // {
+    //   title: "tests-1",
+    //   body: "asfdfsd",
+    //   journal: "food"
+    // },
+    // {
+    //   title: "test-2",
+    //   body: "sdjlfkjdsklf",
+    //   journal: "excercise"
+    // },
+    // {
+    //   title: "test-3",
+    //   body: "dude",
+    //   journal: "food"
+    // }
+  ];
+  
+  // Make the array accessible to other files using require.
+  module.exports = journalArray;
